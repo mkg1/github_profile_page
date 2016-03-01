b1882eba2a37b82f43777f9e8167b4ac4c853b8b
Molly G <gehring.mg@gmail.com>
2016-03-01 16:45:08 -0500

    Print list of repository names to the browser

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,38 @@ class User
     @user_name = HTTParty.get("https://api.github.com/users/#{user_name}/repos")
   end
 
-  def show_repos
-    repos = []
+  # def show_repos
+  #   repos = []
+  #   @user_name.each do |x|
+  #     repos << x["name"]
+  #   end
+  #   repos
+  # end
+
+  def repo_ids
+    repo_ids = []
     @user_name.each do |x|
-      repos << x["name"]
+      repo_ids << x["id"]
     end
-    repos
+    repo_ids
   end
 
+  def repo_names
+    repo_names = []
+    @user_name.each do |x|
+      repo_names << x["name"]
+    end
+    repo_names
+  end
+
+  def repo_full_names
+    repo_full_names = []
+    @user_name.each do |x|
+      repo_full_names << x["name"]
+    end
+    repo_full_names
+  end
 end
+
+user = User.new("mkg1")
+p user.repo_ids
