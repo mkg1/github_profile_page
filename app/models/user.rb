7474800be2f615019ed95251f758b20a438162dd
Molly G <gehring.mg@gmail.com>
2016-03-01 17:19:09 -0500

    Display user's full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ require 'byebug'
 # require "#{Rails.root}/test/fixtures/playlist.json"
 
 class User
-  attr_reader :user_name
+  attr_reader :user_name, :user
   def initialize(user_name)
     # @search_term = JSON.parse(File.read("./test/fixtures/repos.json"))
-    @user_name = HTTParty.get("https://api.github.com/users/#{user_name}/repos")
+    @user_repos = HTTParty.get("https://api.github.com/users/#{user_name}/repos")
+    @user = HTTParty.get("https://api.github.com/users/#{user_name}")
   end
-
   # def show_repos
   #   repos = []
   #   @user_name.each do |x|
@@ -17,9 +17,13 @@ class User
   #   repos
   # end
 
+  def user_full_name
+    @user["name"]
+  end
+
   def repo_ids
     repo_ids = []
-    @user_name.each do |x|
+    @user_repos.each do |x|
       repo_ids << x["id"]
     end
     repo_ids
@@ -27,7 +31,7 @@ class User
 
   def repo_names
     repo_names = []
-    @user_name.each do |x|
+    @user_repos.each do |x|
       repo_names << x["name"]
     end
     repo_names
@@ -35,12 +39,12 @@ class User
 
   def repo_full_names
     repo_full_names = []
-    @user_name.each do |x|
+    @user_repos.each do |x|
       repo_full_names << x["name"]
     end
     repo_full_names
   end
 end
 
-user = User.new("mkg1")
-p user.repo_ids
+# user = User.new("masonfmatthews")
+# user.user_full_name
